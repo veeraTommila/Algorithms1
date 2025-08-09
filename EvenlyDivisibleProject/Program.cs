6cf0d8426c18e004ff8ce94a929c83d75324dd13
Veera Tommila <veera.tommila@gmail.com>
2025-08-09 18:23:01 +0300

    Design an algorithm that checks whether an integer is evenly divisible by another integer without using any division operations.
    First, find out that the given numbers a, b, and c are integers.
    There are no commas or periods in integers. Multiply the integers b and
    c together so that the result is a. Do this by comparing the products b *
    c and c * b with each other. Everything is fine if the products are the same for both
    .

---
+++ EvenlyDivisibleProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EvenlyDivisibleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Anna luku:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Anna luku:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Minkä tulon haluat?");
+            int c = Convert.ToInt32(Console.ReadLine()); ;
+            //int jakojaannos = a % b;
+            //while (jaettava >= jakaja)
+            //{
+            //    jaettava = jaettava - jakaja;
+            //}
+            if (b * a == c || a * b == c)
+                Console.WriteLine("Luvut " + a + " ja " + b + " voidaan kertoa keskenään luvulle " + c);
+            else
+                Console.WriteLine("Lukuja " + a + " ja " + b + " ei voida kertoa keskenään luvulle " + c);
+        }
+    }
+}
