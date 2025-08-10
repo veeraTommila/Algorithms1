06e20d41cee7497cc903a3c51e679e1c1ba06030
Veera Tommila <veera.tommila@gmail.com>
2025-08-10 23:18:36 +0300

    Create Program.cs
    Created the first C# codes for the Demo2bConsoleApp.

---
+++ Demo2bConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo2bConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
