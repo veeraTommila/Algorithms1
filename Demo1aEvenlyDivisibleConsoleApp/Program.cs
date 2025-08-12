2e5f78ee4dc721f1bef49890f7499692bbfe777f
Veera Tommila <veera.tommila@gmail.com>
2025-08-12 14:43:35 +0300

    I created a new project, copied the code from the old project's program file into the project's program file, and deleted the old project.
    I created a new project called Demo1aEvenlyDivisibleConsoleApp, copied the code from the old project (called EvenlyDivisibleProject) into the project's program file, and deleted the old project EvenlyDivisibleProject.

---
+++ Demo1aEvenlyDivisibleConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Demo1aEvenlyDivisibleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What output do you want?");
+            int c = Convert.ToInt32(Console.ReadLine()); ;
+
+            if (b * a == c || a * b == c)
+                Console.WriteLine("The numbers " + a + " and " + b + " can be multiplied together to get " + c);
+            else
+                Console.WriteLine("The numbers " + a + " and " + b + " cannot be multiplied together to get " + c);
+        }
+    }
+}
--- Demo2bConsoleApp/Program.cs
+++ Demo2bConsoleApp/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace Demo2bConsoleApp
 {
+    /* What does this code do?
+     * The code removes duplicates from a sorted array by replacing them with zero and counts how many numbers were zeroed. 
+     * It initializes an array.
+     * It goes trhough the array with a loop.?
+     * It prints the array and the number of zeroed numbers.
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbertable = { 1, 2, 3, 3, 4, 4, 5, 6, 7, 7, 8, 9, 10 }; // Initializing the table.           
+            int numberOfZeros = 0;  // Initializing the counter.
+
+            // Let's go through the table with a single loop.
+            for (int i = 1; i < numbertable.Length; i++)
+            {
+
+                if (numbertable[i] == numbertable[i - 1])
+                {
+                    numbertable[i] = 0;
+                    numberOfZeros++;
+                }
+            }
+
+            // Printing the modified table.
+            Console.WriteLine("Modified table:");
+            foreach (int number in numbertable)
+            {
+                Console.WriteLine(number + " ");
+            }
+
+            // The number of reset entries is being printed.
+            Console.WriteLine("\nNumber of zeroed numbers: " + numberOfZeros);
         }
     }
 }
